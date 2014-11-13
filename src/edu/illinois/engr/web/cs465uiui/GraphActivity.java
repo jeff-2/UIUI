e56cb2d91cac4d69ab7be0dbca4c8e715d5a60f9
derek-v <derek.v@github.com>
2014-11-13 09:14:25 -0600

    graph navigation (untested)

--- src/edu/illinois/engr/web/cs465uiui/GraphActivity.java
+++ src/edu/illinois/engr/web/cs465uiui/GraphActivity.java
@@ -12,16 +12,13 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 /**Shows a graph of the crowdedness of a restaurant over a day, and lets the user switch days.
  * The intent used to start this activity must be provided to setup().*/
 public class GraphActivity extends Activity
 {
-	/**How many bars we display per day.*/
-	private static final int BARS = 12;
-	
-	
 	/**Put a restaurant ID and initial date into the intent that will be used to start this activity.*/
 	public static void setup(long restaurantID, Calendar date, Intent intent)
 	{
@@ -41,8 +38,12 @@ public class GraphActivity extends Activity
 	
 	
 	
+	/**The date we're currently looking at.*/
+	private Calendar date;
+	
 	private TextView name, location;
 	private CrowdGraph graph;
+	private ViewGroup navBar;
 	
 	
 	@Override protected void onCreate(Bundle savedInstanceState)
@@ -53,8 +54,26 @@ public class GraphActivity extends Activity
 		graph = (CrowdGraph)findViewById(R.id.act_graph_graph);
 		name = (TextView)findViewById(R.id.act_graph_name);
 		location = (TextView)findViewById(R.id.act_graph_location);
+		navBar = (ViewGroup)findViewById(R.id.act_graph_navbar);
 		
 		new LoadTask(intentDate(), intentRestaurantId(), this).execute();
+		refreshNav();
+	}
+	
+	
+	/**Refreshes the dates displayed in the nav bar.*/
+	private void refreshNav()
+	{
+		navBar.removeAllViews();
+		Calendar current = (Calendar)date.clone();
+		current.add(-4, DATE);
+		for(int c = 0; c < 7; c++)
+		{
+			current.add(1, DATE);
+			View v = getLayoutInflater().inflate(R.layout.sub_graph_navday, null);
+			v.setOnClickListener(new NavHandler((Calendar)current.clone(), this));
+			navBar.addView(v);
+		}
 	}
 	
 	
@@ -97,6 +116,25 @@ public class GraphActivity extends Activity
 			}
 			else
 				UIFetch.explainError(result.error, activity);
+		}
+	}
+	
+	
+	/**Handles clicks on the nav element with the provided date.*/
+	private class NavHandler implements View.OnClickListener
+	{
+		private final Calendar myDate;
+		private final Activity activity;
+		public NavHandler(Calendar date, Activity activity)
+		{
+			this.myDate = date;
+			this.activity = activity;
+		}
+		
+		@Override public void onClick(View v)
+		{
+			date = myDate;
+			new LoadTask(myDate, intentRestaurantId(), activity);
 		}
 	}
 }
